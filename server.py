23652a5c8f88a9f7c1affab2e75ca63f7e74c39d
cristianantinao <cristiangabrielantinao000@gmail.com>
2023-12-12 19:41:25 -0300

    asignacion juego de numeros genial

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+app = Flask(__name__)  
+app.secret_key = 'secret'
+
+numero_secreto = random.randint(0,100)
+
+
+
+@app.route('/')         
+def index():
+    if 'numero_secreto' not in session:
+            session['numero_secreto'] = random.randint(1, 100)
+            print(session['numero_secreto'])
+    return render_template('index.html')
+
+
+@app.route("/respuesta", methods=["POST"])
+def respuesta():
+    
+    numero_adivinado = int(request.form['numero_respuesta'])
+    numero_secreto = session['numero_secreto']
+        
+    if numero_adivinado < numero_secreto:
+        mensaje = 'Demasiado Bajo'
+    elif numero_adivinado > numero_secreto:
+        mensaje = 'Demasiado Alto'
+    else:
+        mensaje = '¡Correcto! El número era ' + str(numero_secreto)
+        session.pop('numero_secreto')
+
+    return render_template('index.html', mensaje=mensaje)
+
+
+if __name__=="__main__":   
+    app.run(debug=True)
